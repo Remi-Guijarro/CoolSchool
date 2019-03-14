9264421d42201c76846bfc78b61d4fb47160b698
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-03-14 11:39:07 +0100

    Added the source directory and the class Subject

---
+++ sources/Objects/Subject.py
@@ -0,0 +1,7 @@
+class Subject(name,coeff,weight,chapters):
+    def __init__(name,coeff,weight,chapters):
+        self.name = name
+        self.coeff = coeff
+        self.weight =weight
+        self.chapters = chapters
+
