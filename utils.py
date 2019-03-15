8123f17b8fabb985d321451b56af8514d1db2223
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-03-15 08:05:42 +0100

    Added get_subjects() function that read the csv file and return a list of objects (dictionnary)

--- utils.py
+++ utils.py
@@ -1,5 +1,25 @@
+import csv
+
+
 def compute_stat(user_name):
     print("getting stat")
 
+#save the score of the current 'player' in the current mode
 def save():
     print("writing file")
+
+#return a list of objects ( dictionnary )
+def get_subjects():
+    objectsList=[]
+    with open('subjects.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            line={}
+            line["subject"]=row[0]
+            line["chapter"]=row[1]
+            line["theme"]=row[2]
+            line["structure"]=row[3]
+            objectsList.append(line)
+        print(objectsList)
+
+get_subjects()
