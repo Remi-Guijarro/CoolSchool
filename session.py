8c6c9b815830e6b5661c20f013b82da4199dc593
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2019-03-15 16:00:22 +0100

    Corrected session.py: forgot save

--- session.py
+++ session.py
@@ -1,4 +1,15 @@
 import interface
+import utils
 
 def run():
-	print("run")
+	mode = interface.chooseMode()
+
+	intitules=("Second degré","Intégrales","Fonctions puissance","Fonctions trigonométriques","Fonctions logarithmiques")
+	probabilites = interface.chooseProbabilities(intitules)
+
+	intitules_proba=[]
+	for i in range(0, 5):
+		intitules_proba.append((intitules[i],probabilites[i])
+
+	# Main loop
+	
