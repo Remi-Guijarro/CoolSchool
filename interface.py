57eb43724f068f177afc2e5b781a5ef16797ba34
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-03-15 15:50:30 +0100

    refact and added chooseproba function

--- interface.py
+++ interface.py
@@ -4,3 +4,11 @@ MODE2="TRAINING"
 def chooseMode():
     mode = raw_input("Which mode do you want to run ? ("+ MODE1 + "|" + MODE2 + ")")
     return mode
+
+
+def chooseProbabilities(n_uplet):
+    lp=[]
+    for intitule in n_uplet:
+        proba = raw_input("Choose the propabilities for : " + intitule)
+        lp.append(proba)
+    return lp
