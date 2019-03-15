d0c211bd9adc6fdc1d26a00037b1dcdfa16e7db2
Remi-Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2019-03-15 07:49:48 +0100

    Added const MODE1 & MODE2, also change the structure of the file subject in a more simple way (CSV)

--- interface.py
+++ interface.py
@@ -1,4 +1,7 @@
+MODE1="EXAM"
+MODE2="TRAINING"
 
 def chooseMode():
-    mode = raw_input("Which mode do you want to run ? (exam|training)")
+    mode = raw_input("Which mode do you want to run ? ("+ MODE1 + "|" + MODE2 + ")")
     return mode
+
