4fd6ce30c887beb641b1b3fcd89df4f8c0540c98
Remi Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-03-16 17:14:50 +0100

    Added a fucntion to retrieve a random number

--- utils.py
+++ utils.py
@@ -1,15 +1,20 @@
 import operator
+import random
 
 def func_repart(intitules_proba):
     intitules_proba.sort(key = operator.itemgetter(1))
     print(intitules_proba)
-    # repartition=[]
-    # for i in range(0, len(intitules_proba)):
-    #     repartition.append(get_coef_cumul(intitules_proba,i)/get_coef_cumul(intitules_proba,len(intitules_proba)))
-    # return repartition
+    repartition=[]
+    for i in range(0, len(intitules_proba)):
+        repartition.append(get_coef_cumul(intitules_proba,i)/get_coef_cumul(intitules_proba,len(intitules_proba)))
+    return repartition
 
 def get_coef_cumul(intitules_proba, index):
     coef_cumul = 0
     for i in range(0, index):
-        coef_cumul += intitules_proba[i][1]
+        coef_cumul = coef_cumul + float(intitules_proba[i][1])
     return coef_cumul
+
+
+def get_rand():
+    return random.uniform(0, 1)
