4482fac2e774291b390de52c24d64d4137c89333
Remi Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-03-16 16:58:10 +0100

    bug fix

--- interface.py
+++ interface.py
@@ -2,13 +2,13 @@ MODE1="EXAM"
 MODE2="TRAINING"
 
 def chooseMode():
-    mode = raw_input("Which mode do you want to run ? ("+ MODE1 + "|" + MODE2 + ")")
+    mode = input("Which mode do you want to run ? ("+ MODE1 + "|" + MODE2 + ")")
     return mode
 
 
 def chooseProbabilities(n_uplet):
     lp=[]
     for intitule in n_uplet:
-        proba = raw_input("Choose the propabilities for : " + intitule)
+        proba = input("Choose the propabilities for : " + intitule)
         lp.append(proba)
     return lp
