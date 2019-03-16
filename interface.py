39d95300f6c8e5d4acf4c8df5a62e98a202eecb2
Remi Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-03-16 17:39:33 +0100

    change prompt for mode and added some colors display for errors

--- interface.py
+++ interface.py
@@ -1,8 +1,20 @@
+from colorama import init, Fore, Back, Style
+
 MODE1="EXAM"
 MODE2="TRAINING"
 
+#  return 1 if Exam mode have been choosed 0 otherwise
 def chooseMode():
-    mode = input("Which mode do you want to run ? ("+ MODE1 + "|" + MODE2 + ")")
+    init(convert=True)
+    while(True):
+        print(Style.RESET_ALL)
+        mode = input("Which mode do you want to run ? ("+ MODE1 + "|" + MODE2 + ") :> ")
+        if(mode.upper() == MODE1):
+            return 1
+        elif(mode.upper() == MODE2):
+            return 0
+        else:
+            print(Fore.RED + "The given mode does not exist \n" )
     return mode
 
 
