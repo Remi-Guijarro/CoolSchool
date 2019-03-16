51aa10088df72d283e13eaf63f837136e549dfbe
Remi Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-03-16 16:59:12 +0100

    refacting functions

--- session.py
+++ session.py
@@ -1,15 +1,19 @@
 import interface
-import utils
+from  utils import *
 
 def run():
 	mode = interface.chooseMode()
 
 	intitules=("Second degré","Intégrales","Fonctions puissance","Fonctions trigonométriques","Fonctions logarithmiques")
 	probabilites = interface.chooseProbabilities(intitules)
+	intitule_proba_mat=[]
+	intitules_proba_chapters=[]
+	for i in range(0, 2):
+		intitule_proba_mat.append((intitules[i],probabilites[i]))
+	for i in range(2, 5):
+		intitules_proba_chapters.append((intitules[i],probabilites[i]))
+		
+	print(func_repart(intitule_proba_mat))	
+	print(func_repart(intitules_proba_chapters))	
 
-	intitules_proba=[]
-	for i in range(0, 5):
-		intitules_proba.append((intitules[i],probabilites[i])
-
-	# Main loop
-	
+run()
