c2ad83c9bd50762ddd219c1cb5d55e7b73f70a57
Remi Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-03-16 16:59:42 +0100

    bug fix

--- utils.py
+++ utils.py
@@ -2,10 +2,11 @@ import operator
 
 def func_repart(intitules_proba):
     intitules_proba.sort(key = operator.itemgetter(1))
-    repartition=[]
-    for i in range(0, intitules_proba.size())
-        repartition.append(coef_cumul(intitules_proba,i)/coef_cumul(intitules_proba,intitules_proba.size()))
-    return repartition
+    print(intitules_proba)
+    # repartition=[]
+    # for i in range(0, len(intitules_proba)):
+    #     repartition.append(get_coef_cumul(intitules_proba,i)/get_coef_cumul(intitules_proba,len(intitules_proba)))
+    # return repartition
 
 def get_coef_cumul(intitules_proba, index):
     coef_cumul = 0
