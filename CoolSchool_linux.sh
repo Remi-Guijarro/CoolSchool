bc8c203de8611481cd5f2c0d6d6d0bd21d11b668
Geoffrey Delval <delval.geoffrey@axessvision-tsc.com>
2019-03-28 13:32:53 +0100

    Update linux script
    Only install libraries the user has not already installed

--- CoolSchool_linux.sh
+++ CoolSchool_linux.sh
@@ -1,5 +1,33 @@
-pip3 install colorama sympy
-pip3 install sympy
-pip3 install numpy
+#!/bin/bash
+
+PROGRAMS=""
+COLORAMA=false
+SYMPY=false
+NUMPY=false
+
+for PIP_LIST_OUTPUT in `pip3 list --format=columns`; do
+    if [ "$PIP_LIST_OUTPUT" == "colorama" ]; then
+		COLORAMA=true
+	elif [ "$PIP_LIST_OUTPUT" == "sympy" ]; then
+		SYMPY=true
+	elif [ "$PIP_LIST_OUTPUT" == "numpy" ]; then
+		NUMPY=true
+	fi
+done
+
+if [ $COLORAMA == false ]; then
+	PROGRAMS+="colorama "
+fi
+if [ $SYMPY == false ]; then
+	PROGRAMS+="sympy "
+fi
+if [ $NUMPY == false ]; then
+	PROGRAMS+="numpy "
+fi
+
+if [ ! -z "$PROGRAMS" ]; then
+	pip3 install $PROGRAMS
+fi
+
 clear
 python3 CoolSchool.py
