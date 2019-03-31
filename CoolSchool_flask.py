2bc88cea2db3d932d67fa4389e5ab7bb92070a3d
Remi Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-03-31 23:08:07 +0200

    Added bootstrap and fisrt page template and route

--- CoolSchool_flask.py
+++ CoolSchool_flask.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template,url_for
+from interface import chooseChapterProbabilities,chooseSubjectProbabilities
+from session import constructIntituleMa,constructIntituleChapters
+from pprint import pprint
+
 app = Flask(__name__)
 
+intitules=["M_Polynome","\tPolynome_Second degre","M_Integrales","\tIntegrales_Fonctions puissance","\tIntegrales_Fonctions trigonometriques","\tIntegrales_Fonctions logarithmiques"]
+subjectsArray,subjectsMap=constructIntituleMa(intitules)
+chaptersMap= constructIntituleChapters(intitules,subjectsArray)
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    pprint(chaptersMap)
+    return render_template('home.html',chapters=chaptersMap)
--- CoolSchool_win.bat
+++ CoolSchool_win.bat
@@ -13,7 +13,7 @@ set /p bool_flask_app=Do you want to run the flask app version [y/n]?:
 if [%bool_flask_app%] equ [y] (
 	set FLASK_APP=CoolSchool_flask.py
 	set FLASK_DEBUG=1
-	flask run
+	flask run 
 ) else (
 	python ./CoolSchool.py
     cmd /k
